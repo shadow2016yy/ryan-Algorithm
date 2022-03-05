9b3fe8c3c9c9000c5e167cce945072ec18c2f622
chenluming <chenluming@jd.com>
2022-03-05 23:20:42 +0800

    初始化项目

---
+++ src/main/java/com/youzi/ryanalgorithm/RyanAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.youzi.ryanalgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyanAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RyanAlgorithmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youzi/ryanalgorithm/RyanAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youzi.ryanalgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyanAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
